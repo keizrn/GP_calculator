0e62e35a8917bee32efad7bed86f0a91c1d86c3e
Makcmay <101573637+Makcmay@users.noreply.github.com>
2022-11-17 13:28:42 +0300

    add  def description

--- view.py
+++ view.py
@@ -6,20 +6,27 @@ def view_data(data,value_a, value_b, action):
 
 
 def get_float_value():
+    '''Пользователь вводит число'''
     return Decimal(input('value = '))
 
 
 def get_complex_value():
+    '''Пользователь вводит комплексное число
+    в формате a+bj, можно ввести только (a) тогда формат a+0j'''
     return complex(input('value = '))
 
 def new_line():
     print()
 
 def get_action():
+    '''Пользователь вводит действие +-*/'''
     return str(input('Введите действие +-*/  = '))
 
 
 def get_number_info():
+    '''Выбор типа данных для работы,
+    комплексные = 1, рациональные = 2
+    выход из программы = 3'''
     return int(input('Введите тип данных:\n'
                      'Комплексные = 1\n'
                      'Рациональные = 2\n'
@@ -29,11 +36,11 @@ def get_number_info():
 def menu_error():
     print('Ошибка выбора меню')
 
-def input_error(input_text):
+def input_error():
     flag = False
     while not flag:
         try:
-            value = int(input(f'{input_text}'))
+            value = int(input('value = '))
             flag = True
         except ValueError:
                 print('Введено не число')
